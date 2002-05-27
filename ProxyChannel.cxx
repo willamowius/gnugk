eac5f6230acda9072e194490c3c45078262a7060
mstormi <>
2002-05-27 10:08:41 +0000

    fix compilation (typo)

--- ProxyChannel.cxx
+++ ProxyChannel.cxx
@@ -1242,7 +1242,7 @@ void CallSignalSocket::CgPNConversion(Q931 &q931pdu, H225_Setup_UUIE &setup) {
 					    &si
 				    );
 
-		if (NULL != callrec && !callRec->GetCallingProfile().isCPE()) {
+		if (callRec && !callRec->GetCallingProfile().isCPE()) {
 			PTRACE(5, "EP not from CPE");
 
 			if (cgPNIncluded) {
