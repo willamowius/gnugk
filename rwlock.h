bdb32b89374ac91d8e2148257770b93e764b2300
nilsb <>
2002-06-27 08:41:40 +0000

    Imported from 2.0-version

---
+++ rwlock.h
@@ -0,0 +1,55 @@
+//////////////////////////////////////////////////////////////////
+//
+// rwlock.h
+//
+// Copyright (c) Citron Network Inc. 2002
+//
+// This work is published under the GNU Public License (GPL)
+// see file COPYING for details.
+// We also explicitely grant the right to link this code
+// with the OpenH323 library.
+//
+// initial author: Chin-Wei Huang <cwhuang@linux.org.tw>
+// initial version: 6/7/2002
+//
+//////////////////////////////////////////////////////////////////
+
+#ifndef __rwlock_h__
+#define __rwlock_h__
+
+#include <ptlib.h>
+
+// Utility for PReadWriteMutex usage
+
+class ReadLock {
+	PReadWriteMutex & mutex;
+  public:
+	ReadLock(PReadWriteMutex & m) : mutex(m) { mutex.StartRead(); }
+	~ReadLock() { mutex.EndRead(); }
+};
+
+class WriteLock {
+	PReadWriteMutex & mutex;
+  public:
+	WriteLock(PReadWriteMutex & m) : mutex(m) { mutex.StartWrite(); }
+	~WriteLock() { mutex.EndWrite(); }
+};
+
+class ReadUnlock {
+	PReadWriteMutex & mutex;
+  public:
+	ReadUnlock(PReadWriteMutex & m) : mutex(m) { mutex.EndRead(); }
+	~ReadUnlock() { mutex.StartRead(); }
+};
+
+class WriteUnlock {
+	PReadWriteMutex & mutex;
+  public:
+	WriteUnlock(PReadWriteMutex & m) : mutex(m) { mutex.EndWrite(); }
+	~WriteUnlock() { mutex.StartWrite(); }
+};
+
+extern PReadWriteMutex ConfigReloadMutex;
+
+#endif // __rwlock_h__
+
