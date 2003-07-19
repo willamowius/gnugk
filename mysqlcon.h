881bfe848f0dc0c4c835ac2f90c478603c870e40
cwhuang <>
2003-07-19 05:20:25 +0000

    first import for 2.2

---
+++ mysqlcon.h
@@ -0,0 +1,66 @@
+//////////////////////////////////////////////////////////////////
+//
+// MySQL Connection for GNU Gatekeeper
+//
+// Copyright (c) Citron Network Inc. 2002-2003
+//
+// This work is published under the GNU Public License (GPL)
+// see file COPYING for details.
+// We also explicitely grant the right to link this code
+// with the OpenH323 library.
+//
+// initial author: Chih-Wei Huang <cwhuang@linux.org.tw>
+// initial version: 07/15/2003
+//
+//////////////////////////////////////////////////////////////////
+
+#ifndef MYSQLCON_H
+#define MYSQLCON_H "@(#) $Id$"
+
+#ifndef _PTLIB_H
+#include <ptlib.h>
+#endif
+
+
+struct st_mysql;
+struct st_mysql_res;
+typedef st_mysql MYSQL;
+typedef st_mysql_res MYSQL_RES;
+
+class MySQLConnection {
+public:
+	class Result {
+	public:
+		Result(MYSQL_RES *r = 0) : m_result(r) {}
+		~Result();
+
+		operator MYSQL_RES *() { return m_result; }
+		bool Store(MYSQL *, const char *);
+
+	private:
+		MYSQL_RES *m_result;
+	};
+
+	MySQLConnection(PConfig *, const char *);
+	virtual ~MySQLConnection();
+
+	bool Query(const PString &, Result &);
+	bool Query(const PString &, PStringArray &);
+	bool Query(const PString &, PString &);
+
+protected:
+	PString GetSelectClause(const PString &) const;
+
+private:
+	bool Init();
+	void Cleanup();
+
+	PMutex m_mutex;
+	MYSQL *m_connection;
+	PString m_clause;
+
+	PConfig *m_config;
+	const char *m_section;
+};
+
+#endif // MYSQLCON_H
---
+++ slist.h
@@ -0,0 +1,61 @@
+//////////////////////////////////////////////////////////////////
+//
+// slist.h
+//
+// A Single Linked List Template
+//
+// Copyright (c) Citron Network Inc. 2003
+//
+// This work is published under the GNU Public License (GPL)
+// see file COPYING for details.
+// We also explicitely grant the right to link this code
+// with the OpenH323 library.
+//
+// initial author: Chih-Wei Huang <cwhuang@linux.org.tw>
+// initial version: 07/16/2003
+//
+//////////////////////////////////////////////////////////////////
+
+#ifndef SLIST_H
+#define SLIST_H "@(#) $Id$"
+
+#ifndef FACTORY_H
+#include "factory.h"
+#endif
+
+template<class T>
+class SList {
+public:
+	typedef T Base;	// for SimpleCreator template 
+
+	SList() : m_next(0) {}
+	virtual ~SList() = 0;
+
+	T* GetNext() { return m_next; }
+	const T* GetNext() const { return m_next; }
+
+	static T *Create(const PStringArray &);
+	
+protected:
+	T *m_next;
+};
+
+template<class T>
+SList<T>::~SList()
+{
+	delete m_next;  // delete whole list recursively
+}
+
+template<class T>
+T *SList<T>::Create(const PStringArray & rules)
+{
+	T *next = 0;
+	for (int i = rules.GetSize(); --i >= 0; )
+		if (T *current = Factory<T>::Create(rules[i])) {
+			current->m_next = next;
+			next = current;
+		}
+	return next;
+}
+
+#endif // SLIST_H
