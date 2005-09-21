d730370c64030770bb9d5901364220cb75c074f4
zvision <>
2005-09-21 10:39:02 +0000

    dumpdata/restoredata script errors fixed

--- contrib/sqlbill/sql/bin/dumpdata.sh
+++ contrib/sqlbill/sql/bin/dumpdata.sh
@@ -3,15 +3,16 @@
 # This script extracts data from the database.
 # Compressed dump is sent to stdout. Use restore.sh script to restore
 # data from a generated dump file.
+# IMPORTANT: This scripts dumps DATA ONLY. Schema is not saved, so in order
+# to perform full database restoration, you need first to create the schema
+# and then run restore.sh to fill it with data.
 #
 # Usage:
 #   dumpdata > dump
 
-echo "Dumping data from the database..."
-
 DBUSER=gkradius
 DBSERVER=localhost
 DBEXTRAOPTS=
-DUMPTYPE="-a -Fc -Z 9"
+DUMPTYPE="-a -Fc -Z 9 -v"
 
-pg_dump $DUMPTYPE -U $DBUSER -h $DBSERVER $DBEXTRAOPTS voipdb
+$DUMPTYPE -U $DBUSER -h $DBSERVER $DBEXTRAOPTS voipdb
--- contrib/sqlbill/sql/bin/restoredata.sh
+++ contrib/sqlbill/sql/bin/restoredata.sh
@@ -3,16 +3,15 @@
 # This script restores data from a previously generated database dump.
 # The database should be empty (no data, only table definitions) prior
 # to the restore operation. Use dumpdata.sh script to dump data to a file.
+# IMPORTANT: The restore operation has to be executed as PostgreSQL superuser
+# in order to be able to disable triggers.
 #
 # Usage:
 #   restoredata < dump
 
-echo "Restoring data from the database dump..."
-
-DBUSER=gkradius
-DBSUPERUSER=postgres
+DBUSER=postgres
 DBSERVER=localhost
 DBEXTRAOPTS=
-DUMPTYPE="-a --disable-triggers"
+DUMPTYPE="-a --disable-triggers -v"
 
-pg_restore $DUMPTYPE -S $DBSUPERUSER -U $DBUSER -h $DBSERVER $DBEXTRAOPTS -d voipdb
+pg_restore $DUMPTYPE -U $DBUSER -h $DBSERVER $DBEXTRAOPTS -d voipdb
