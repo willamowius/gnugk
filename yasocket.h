6ca10400370e297fd59af2656f142ffb56baa7c2
Jan Willamowius <jan@willamowius.de>
2006-03-22 10:37:20 +0000

    fix compilation with gcc 4.1

--- yasocket.h
+++ yasocket.h
@@ -412,7 +412,7 @@ class TCPListenSocket : public TCPSocket {
 	PCLASSINFO ( TCPListenSocket, TCPSocket )
 #endif
 public:
-	TCPListenSocket::TCPListenSocket(int seconds = 0);
+	TCPListenSocket(int seconds = 0);
 	~TCPListenSocket();
 
 	bool IsTimeout(const PTime *) const;
