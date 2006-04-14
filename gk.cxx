997eb5a1984acb688862db3b08f47e3edfdbce1f
Jan Willamowius <jan@willamowius.de>
2006-04-14 14:08:57 +0000

    correct spelling of FourtyTwo entry to allow correct spelling of FortyTwo also

--- gk.cxx
+++ gk.cxx
@@ -407,11 +407,12 @@ BOOL Gatekeeper::InitConfig(const PArgList &args)
 
 	InstanceOf<Toolkit>()->SetConfig(fp, section);
 
-	if( (GkConfig()->GetInteger("Fourtytwo") ) != 42) { 
+	if( (GkConfig()->GetInteger("Fortytwo") ) != 42) &&
+		(GkConfig()->GetInteger("Fourtytwo") ) != 42) { 
 		cerr << "WARNING: No config file found!\n"
 			 << "- Does the config file exist? The default (~/.pwlib_config/Gatekeeper.ini or gatekeeper.ini in current directory) or the one given with -c?\n"
 			 << "- Did you specify they the right 'Main' section with -s?\n" 
-			 << "- Is the line 'Fourtytwo=42' present in this 'Main' section?"<<endl;
+			 << "- Is the line 'Fortytwo=42' present in this 'Main' section?"<<endl;
 	}
 	
 	return CheckSectionName(GkConfig());
