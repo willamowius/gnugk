561313f8987320e9c0b855c89f509a74b8393884
Jan Willamowius <jan@willamowius.de>
2008-01-17 11:26:34 +0000

    use rewritten cause code in accounting

--- ProxyChannel.cxx
+++ ProxyChannel.cxx
@@ -2521,6 +2521,7 @@ void CallSignalSocket::OnReleaseComplete(
 				PTRACE(4, "Q931\tTranslated cause code " << cause << " to " << new_cause);
 				msg->GetQ931().SetCause(Q931::CauseValues(new_cause));
 				msg->SetChanged();
+				m_call->SetDisconnectCause(new_cause);
 			}
 
 			m_call->SetDisconnectCause(cause);
