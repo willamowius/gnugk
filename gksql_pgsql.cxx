d3e9f83c752e07a221d6ecb025197a46e71788b9
Simon Horne <s.horne@packetizer.com>
2008-04-19 07:10:21 +0000

    added secur32.lib for windows

--- gksql_pgsql.cxx
+++ gksql_pgsql.cxx
@@ -11,6 +11,9 @@
  * with the OpenH323 library.
  *
  * $Log$
+ * Revision 1.16  2008/04/18 14:37:28  willamowius
+ * never include gnugkbuildopts.h directly, always include config.h
+ *
  * Revision 1.15  2008/04/18 13:14:11  shorne
  * Fixes for auto-configure on windows
  *
@@ -73,6 +76,7 @@
 
 #ifdef _WIN32
 #pragma comment( lib, PGSQL_LIBRARY )
+#pragma comment( lib, "secur32.lib")
 #endif
 
 /** Class that encapsulates SQL query result for PostgreSQL backend.
