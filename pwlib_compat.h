d913791cf8b442d054f6e9a3a5dbe5d5055c3e18
Jan Willamowius <jan@willamowius.de>
2008-09-29 23:09:49 +0000

    activate SRV policy whenever P_DNS is available, no need to check OpenH323 version, fix indentation

--- pwlib_compat.h
+++ pwlib_compat.h
@@ -25,16 +25,16 @@
 
 // define PTimedMutex for PWLib < 1.9.2
 #if PWLIB_MAJOR == 1
-  #if PWLIB_MINOR < 9
-    #define PTimedMutex PMutex
-  #endif
+	#if PWLIB_MINOR < 9
+		#define PTimedMutex PMutex
+	#endif
 #endif
 #if PWLIB_MAJOR == 1
-  #if PWLIB_MINOR == 9
-    #if PWLIB_BUILD < 2
-      #define PTimedMutex PMutex
-    #endif
-  #endif
+	#if PWLIB_MINOR == 9
+		#if PWLIB_BUILD < 2
+			#define PTimedMutex PMutex
+		#endif
+	#endif
 #endif
 
 #if !defined(P_USE_STANDARD_CXX_BOOL) && !defined(P_USE_INTEGER_BOOL)
@@ -42,91 +42,89 @@
 #endif
 
 #ifdef P_DNS
-// define hasRDS for PWLib >= 1.11.3
- #if PWLIB_MAJOR == 1
-  #if PWLIB_MINOR >= 11
-    #if PWLIB_BUILD > 2     
-       #define hasRDS 1
-    #endif
-  #endif
- #endif
+	#define hasSRV 1	             // DNS SRV
+	// define hasRDS for PWLib >= 1.11.3
+	#if PWLIB_MAJOR == 1
+		#if PWLIB_MINOR >= 11
+			#if PWLIB_BUILD > 2     
+				#define hasRDS 1
+			#endif
+		#endif
+	#endif
 
-// define hasSETENUMSERVERS for PWLib >= 1.9.3
- #if PWLIB_MAJOR == 1
-  #if PWLIB_MINOR >= 9
-     #define hasSETENUMSERVERS 1
-  #endif
- #endif
+	// define hasSETENUMSERVERS for PWLib >= 1.9.3
+	#if PWLIB_MAJOR == 1
+		#if PWLIB_MINOR >= 9
+			#define hasSETENUMSERVERS 1
+		#endif
+	#endif
 #endif
 
 // define hasDeletingSetStream for PWLib >= 1.11.2
 #if PWLIB_MAJOR == 1
-  #if PWLIB_MINOR >= 11
-    #if PWLIB_BUILD >= 2
-       #define hasDeletingSetStream 1
-    #endif
-  #endif
+	#if PWLIB_MINOR >= 11
+		#if PWLIB_BUILD >= 2
+			#define hasDeletingSetStream 1
+		#endif
+	#endif
 #endif
 
 #if PWLIB_MAJOR == 1
- #if PWLIB_MINOR == 12
-     #define hasRDS 1 
-     #define hasSETENUMSERVERS 1    
- #endif
+	#if PWLIB_MINOR == 12
+		#define hasRDS 1 
+		#define hasSETENUMSERVERS 1    
+	#endif
 #endif
 
 // for Ptlib v2.x
 #ifdef PTLIB_MAJOR 
-  #define hasRDS 1 
-  #define hasSETENUMSERVERS 1    
-  #define hasDeletingSetStream 1
+	#define hasRDS 1 
+	#define hasSETENUMSERVERS 1    
+	#define hasDeletingSetStream 1
 #endif
 
 // changed PConfig interface in PWLib >= 2.2.0
 #if PTLIB_MAJOR == 2
-  #if PTLIB_MINOR >= 2
-	#define hasPConfigArray 1
-  #endif
+	#if PTLIB_MINOR >= 2
+		#define hasPConfigArray 1
+	#endif
 #endif
 
 #if !defined(PWLIB_MAJOR) && !defined(PTLIB_MAJOR)
-  #if _WIN32
-    #pragma message ("warning: Can't detect PWLib/PTLib version")
-  #else
-    #warning "Can't detect PWLib/PTLib version"
-  #endif
-  // be on the safe side and rsik a small memleak instead of a crash
-  #define hasDeletingSetStream 1
+	#if _WIN32
+		#pragma message ("warning: Can't detect PWLib/PTLib version")
+	#else
+		#warning "Can't detect PWLib/PTLib version"
+	 #endif
+	// be on the safe side and risk a small memleak instead of a crash
+	#define hasDeletingSetStream 1
 #endif
 
 ///////////////////////////////////////////////
 
 // OpenH323 version matching
 #if OPENH323_MAJOR == 1
-    #if OPENH323_MINOR >= 19
-       #if OPENH323_MINOR == 19
-	   #if OPENH323_BUILD > 0
-	   #define OpenH323Factory 1         // OpenH323 Factory Loader Auth
-	   #endif
-	   #if OPENH323_BUILD > 4
-                #define h323pluslib 1        // Indicate H323plus Library
-		#ifdef H323_H460
-			#define hasH460 1    // H460 support
+	#if OPENH323_MINOR >= 19
+		#if OPENH323_MINOR == 19
+			#if OPENH323_BUILD > 0
+				#define OpenH323Factory 1         // OpenH323 Factory Loader Auth
+			#endif
+			#if OPENH323_BUILD > 4
+				#define h323pluslib 1        // Indicate H323plus Library
+				#ifdef H323_H460
+					#define hasH460 1    // H460 support
+				#endif
+				#define h323v6 1             // Version 6 features
+			#endif
+		#else // h323plus v1.20
+			#define h323pluslib 1             // Indicate H323plus Library
+			#define OpenH323Factory 1         // OpenH323 Factory Loader Auth 
+			#define h323v6 1                  // Version 6 features  
+			#ifdef H323_H460
+				#define hasH460 1	             // H460 support 
+			#endif
 		#endif
-		#define h323v6 1             // Version 6 features
-           #endif
-       #else // h323plus v1.20
-           #define h323pluslib 1             // Indicate H323plus Library
- 	   #define OpenH323Factory 1         // OpenH323 Factory Loader Auth 
-           #define h323v6 1                  // Version 6 features  
-	 #ifdef H323_H460
-           #define hasH460 1	             // H460 support 
-         #endif
-	     #ifdef P_DNS
-	       #define hasSRV 1	             // DNS SRV
-	     #endif     
-       #endif
-     #endif	
+	#endif	
 #endif
 
 #endif
