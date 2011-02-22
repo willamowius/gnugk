d91870744fb996517e7d9d440b57de470b3d4127
Jan Willamowius <jan@willamowius.de>
2011-02-22 10:04:53 +0000

    display support for LDAP eg. whether H.350 is avilable

--- version.cxx
+++ version.cxx
@@ -22,7 +22,7 @@
 const PString Toolkit::GKVersion()
 {
 	return PString(PString::Printf,
-		       "Gatekeeper(%s) Version(%s) Ext(pthreads=%d,radius=%d,mysql=%d,pgsql=%d,firebird=%d,odbc=%d,sqlite=%d,large_fdset=%d,crypto/ssl=%d,h46018=%d,h46023=%d)"
+		       "Gatekeeper(%s) Version(%s) Ext(pthreads=%d,radius=%d,mysql=%d,pgsql=%d,firebird=%d,odbc=%d,sqlite=%d,large_fdset=%d,crypto/ssl=%d,h46018=%d,h46023=%d,ldap=%d)"
 		       " H323Plus(%d.%d.%d) PTLib(%d.%d.%d) Build(%s, %s) Sys(%s %s %s)\r\n",
 		       (const unsigned char*)(PProcess::Current().GetManufacturer()),
 		       (const unsigned char*)(PProcess::Current().GetVersion(true)),
@@ -77,6 +77,11 @@ const PString Toolkit::GKVersion()
 				(int)0,
 #endif
 #if HAS_H46023
+				(int)1,
+#else
+				(int)0,
+#endif
+#if P_LDAP
 				(int)1,
 #else
 				(int)0,
