c59849debcf322e6cd2e6da48a8928b444db61fd
Jan Willamowius <jan@willamowius.de>
2011-05-23 19:47:36 +0000

    unit tests comments

--- testrunner.cxx
+++ testrunner.cxx
@@ -1,3 +1,17 @@
+/*
+ * testrunner.cxx
+ *
+ * run all unit tests for the GNU Gatekeeper
+ *
+ * Copyright (c) 2011, Jan Willamowius
+ *
+ * This work is published under the GNU Public License version 2 (GPLv2)
+ * see file COPYING for details.
+ * We also explicitly grant the right to link this code
+ * with the OpenH323/H323Plus and OpenSSL library.
+ *
+ */
+
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
