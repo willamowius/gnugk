aed9772df369a26291f378e8f456ac0aaa5bc643
Jan Willamowius <jan@willamowius.de>
2012-12-07 11:35:27 +0000

    delete unused worker threads after 2 minutes idle time instead of 10 min

--- job.cxx
+++ job.cxx
@@ -23,7 +23,7 @@
 #include "job.h"
 
 // timeout (seconds) for an idle Worker to be deleted
-#define DEFAULT_WORKER_IDLE_TIMEOUT (10*60)		// 10 minutes
+#define DEFAULT_WORKER_IDLE_TIMEOUT (2*60)		// 2 minutes
 
 /** This class represents a thread that performs jobs. It has two states:
     idle and busy. When it accepts a new Job, it becomes busy. When the job
