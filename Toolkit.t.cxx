101ad6965a27ab916c4a53b52100004dd64e8cf9
Jan Willamowius <jan@willamowius.de>
2013-04-16 20:21:19 +0000

    enable unit testing for all source files

---
+++ Toolkit.t.cxx
@@ -0,0 +1,34 @@
+/*
+ * Toolkit.t.cxx
+ *
+ * unit tests for Toolkit.cxx
+ *
+ * Copyright (c) 2013, Jan Willamowius
+ *
+ * This work is published under the GNU Public License version 2 (GPLv2)
+ * see file COPYING for details.
+ * We also explicitly grant the right to link this code
+ * with the OpenH323/H323Plus and OpenSSL library.
+ *
+ */
+
+#include "config.h"
+#include "Toolkit.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+class ToolkitTest : public ::testing::Test {
+protected:
+	ToolkitTest() {
+	}
+	
+	NetworkAddress na;
+};
+
+
+TEST_F(ToolkitTest, NetworkAddress) {
+	EXPECT_STREQ("0.0.0.0/0", na.AsString());
+}
+
+}  // namespace
