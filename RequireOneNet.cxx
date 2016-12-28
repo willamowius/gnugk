89949f7f55b9a4f8bc3468ac884d9c7f6eee7e5d
Jan Willamowius <jan@willamowius.de>
2016-12-28 20:35:08 +0000

    RequireOneNet module

---
+++ RequireOneNet.cxx
@@ -0,0 +1,92 @@
+/*
+ * RequireOneNet.cxx
+ *
+ * accounting module for GNU Gatekeeper used for authentication - make sure one side of the call is within our network
+ *
+ * Copyright (c) 2016, Jan Willamowius
+ *
+ * This work is published under the GNU Public License version 2 (GPLv2)
+ * see file COPYING for details.
+ * We also explicitly grant the right to link this code
+ * with the OpenH323/H323Plus and OpenSSL library.
+ *
+ */
+
+#include "config.h"
+#include <ptlib.h>
+#include <h323pdu.h>
+#include "RequireOneNet.h"
+#include "Toolkit.h"
+
+RequireOneNet::RequireOneNet(const char* moduleName, const char* cfgSecName)
+    : GkAcctLogger(moduleName, cfgSecName)
+{
+	// it is very important to set what type of accounting events
+	// are supported for each accounting module, otherwise the Log method
+	// will no get called
+	SetSupportedEvents(StatusAcctEvents);
+
+	PConfig* cfg = GetConfig();
+	const PString & cfgSec = GetConfigSectionName();
+	PString myNetworkList = cfg->GetString(cfgSec, "Networks", "");
+	PStringArray nets = myNetworkList.Tokenise(",", FALSE);
+	for (PINDEX i = 0; i < nets.GetSize(); ++i) {
+        if (nets[i].Find('/') == P_MAX_INDEX) {
+            // add netmask to pure IPs
+            if (IsIPv4Address(nets[i])) {
+                nets[i] += "/32";
+            } else {
+                nets[i] += "/128";
+            }
+        }
+        m_myNetworks.push_back(NetworkAddress(nets[i]));
+	}
+}
+
+RequireOneNet::~RequireOneNet()
+{
+}
+
+GkAcctLogger::Status RequireOneNet::Log(GkAcctLogger::AcctEvent evt, const callptr & call)
+{
+	// a workaround to prevent processing end on "sufficient" module
+	// if it is not interested in this event type
+	if ((evt & GetEnabledEvents() & GetSupportedEvents()) == 0)
+		return Next;
+
+	if (!call) {
+		PTRACE(1, "RequireOneNet\t" << GetName() << " - missing call info for event " << evt);
+		return Fail;
+	}
+
+	if (call) {
+        PIPSocket::Address ip;
+        WORD port;
+
+/*        if (call->GetCallingParty()) {
+            GetIPFromTransportAddr(call->GetCallingParty()->GetRasAddress(), ip);
+            if (IsInNetworks(ip, m_myNetworks))
+                return Ok;
+        }
+        if (call->GetCalledParty()) {
+            GetIPFromTransportAddr(call->GetCalledParty()->GetRasAddress(), ip);
+            if (IsInNetworks(ip, m_myNetworks))
+                return Ok;
+        }
+*/
+        call->GetSrcSignalAddr(ip, port);
+        if (IsInNetworks(ip, m_myNetworks))
+            return Ok;
+        call->GetDestSignalAddr(ip, port);
+        if (IsInNetworks(ip, m_myNetworks))
+            return Ok;
+	}
+
+	return Fail;
+}
+
+
+namespace {
+	// append status port accounting logger to the global list of loggers
+	GkAcctLoggerCreator<RequireOneNet> StatusAcctCreator("RequireOneNet");
+}
---
+++ RequireOneNet.h
@@ -0,0 +1,59 @@
+/*
+ * RequireOneNet.h
+ *
+ * accounting module for GNU Gatekeeper used for authentication - make sure one side of the call is within our network
+ *
+ * Copyright (c) 2016, Jan Willamowius
+ *
+ * This work is published under the GNU Public License version 2 (GPLv2)
+ * see file COPYING for details.
+ * We also explicitly grant the right to link this code
+ * with the OpenH323/H323Plus and OpenSSL library.
+ *
+ */
+
+#ifndef __REQUIREONENET_H
+#define __REQUIREONENET_H "@(#) $Id$"
+
+#include "gkacct.h"
+
+
+/** Accounting module for the status port.
+	It sends accounting call start/stop/update/connect events
+	to the status port.
+*/
+class RequireOneNet : public GkAcctLogger
+{
+public:
+	enum Constants
+	{
+		/// events recognized by this module
+		StatusAcctEvents = AcctStart | AcctStop | AcctUpdate | AcctConnect | AcctAlert
+	};
+
+	RequireOneNet(
+		/// name from Gatekeeper::Acct section
+		const char* moduleName,
+		/// config section name to be used with an instance of this module,
+		/// pass NULL to use a default section (named "moduleName")
+		const char* cfgSecName = NULL
+		);
+
+	/// Destroy the accounting logger
+	virtual ~RequireOneNet();
+
+	/// overriden from GkAcctLogger
+	virtual Status Log(AcctEvent evt, const callptr & call);
+
+private:
+	RequireOneNet();
+	/* No copy constructor allowed */
+	RequireOneNet(const RequireOneNet &);
+	/* No operator= allowed */
+	RequireOneNet & operator=(const RequireOneNet &);
+
+private:
+	list<NetworkAddress> m_myNetworks;
+};
+
+#endif /* __REQUIREONENET_H */
