0245a4c0208e50b224cf85c76cd2812406daba32
Jan Willamowius <jan@willamowius.de>
2017-06-06 21:02:04 +0000

    modeling file for Coverity

---
+++ coverity_model.cpp
@@ -0,0 +1,19 @@
+// This is the modeling file for Coverity to avaoid false positives.
+// This code is not supposed to be compiled into the GNU Gatekeeper!
+
+struct _ios_fields{
+	int _precision;
+};
+class ios : public _ios_fields{
+	
+	int precision() const { return _precision; }
+	int precision(int newp) {return _precision; }
+	
+};
+
+// byte swap causes false positive for tainted data
+class RadiusPDU {
+	unsigned int GetLength() const { __coverity_tainted_data_sanitize__((void *)&m_length); return m_length; }
+	int m_length;
+};
+
