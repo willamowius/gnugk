a7fd7fa97e0c64e1a42f7522e5ed0192203aae3f
Jan Willamowius <jan@willamowius.de>
2018-01-27 21:48:56 +0100

    cleanup

--- coverity_model.cpp
+++ coverity_model.cpp
@@ -1,14 +1,12 @@
 // This is the modeling file for Coverity to avoid false positives.
 // This code is not supposed to be compiled into the GNU Gatekeeper!
 
-struct _ios_fields{
+struct _ios_fields {
 	int _precision;
 };
-class ios : public _ios_fields{
-	
+class ios : public _ios_fields {
 	int precision() const { return _precision; }
-	int precision(int newp) {return _precision; }
-	
+	int precision(int newp) { return _precision; }
 };
 
 // byte swap causes false positive for tainted data
