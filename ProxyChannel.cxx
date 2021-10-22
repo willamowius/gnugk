7c11610b8758abc8bfc82a62e7ed9634c31737f3
Jan Willamowius <jan@willamowius.de>
2021-10-22 11:50:31 +0200

    make sure new socket is valid

--- ProxyChannel.cxx
+++ ProxyChannel.cxx
@@ -13002,31 +13002,39 @@ void H46019Session::Send(DWORD sendMultiplexID, const IPAndPortAddress & toAddre
             // bind socket to one fixed port, if it fails sending will still work, but every packet will come from a different source port
 		    if (toAddress.GetIP().GetVersion() == 6) {
                 newsocket = ::socket(PF_INET6, SOCK_DGRAM, 0);
-                int enable = 1;
-                (void)::setsockopt(newsocket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
-		        struct sockaddr_in6 srcaddr6;
-                memset(&srcaddr6, 0, sizeof(srcaddr6));
-                srcaddr6.sin6_family = AF_INET6;
-                srcaddr6.sin6_addr = GNUGK_INADDR_ANY;
-                srcaddr6.sin6_port = htons(srcport);
-                if (bind(newsocket, (struct sockaddr *) &srcaddr6, sizeof(srcaddr6)) < 0) {
-                    PTRACE(0, "JW IPv6 bind to port " << srcport << " for multiplex re-try failed: errno=" << errno);
+                if (newsocket > 0) {
+                    int enable = 1;
+                    (void)::setsockopt(newsocket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+                    struct sockaddr_in6 srcaddr6;
+                    memset(&srcaddr6, 0, sizeof(srcaddr6));
+                    srcaddr6.sin6_family = AF_INET6;
+                    srcaddr6.sin6_addr = GNUGK_INADDR_ANY;
+                    srcaddr6.sin6_port = htons(srcport);
+                    if (bind(newsocket, (struct sockaddr *) &srcaddr6, sizeof(srcaddr6)) < 0) {
+                        PTRACE(0, "JW IPv6 bind to port " << srcport << " for multiplex re-try failed: errno=" << errno);
+                    }
                 }
 		    } else {
                 newsocket = ::socket(PF_INET, SOCK_DGRAM, 0);
-                int enable = 1;
-                (void)::setsockopt(newsocket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
-		        struct sockaddr_in srcaddr;
-                memset(&srcaddr, 0, sizeof(srcaddr));
-                srcaddr.sin_family = AF_INET;
-                srcaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-                srcaddr.sin_port = htons(srcport);
-                if (bind(newsocket, (struct sockaddr *) &srcaddr, sizeof(srcaddr)) < 0) {
-                    PTRACE(0, "JW bind to port " << srcport << " for multiplex re-try failed: errno=" << errno);
+                if (newsocket > 0) {
+                    int enable = 1;
+                    (void)::setsockopt(newsocket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+                    struct sockaddr_in srcaddr;
+                    memset(&srcaddr, 0, sizeof(srcaddr));
+                    srcaddr.sin_family = AF_INET;
+                    srcaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+                    srcaddr.sin_port = htons(srcport);
+                    if (bind(newsocket, (struct sockaddr *) &srcaddr, sizeof(srcaddr)) < 0) {
+                        PTRACE(0, "JW bind to port " << srcport << " for multiplex re-try failed: errno=" << errno);
+                    }
                 }
 		    }
-            Send(sendMultiplexID, toAddress, newsocket, data, len, bufferHasRoomForID, gkIP, true); // re-try sending
-            close(newsocket);
+		    if (newsocket > 0) {
+                Send(sendMultiplexID, toAddress, newsocket, data, len, bufferHasRoomForID, gkIP, true); // re-try sending
+                close(newsocket);
+		    } else {
+		        PTRACE(1, "JW Error: creating new socket for multiplex re-try failed");
+		    }
 		}
 #endif // P_LINUX
 	}
