fb39bc683bdd05c297cca6ade50d59c3f9c5221f
Jan Willamowius <jan@willamowius.de>
2026-07-02 14:08:52 +0200

    start 5.16 development

--- version.h
+++ version.h
@@ -22,7 +22,7 @@
 
 /* Minor version number of the gatekeeper */
 #ifndef GNUGK_MINOR_VERSION
-# define GNUGK_MINOR_VERSION 15
+# define GNUGK_MINOR_VERSION 16
 #endif
 
 /* Release status for the gatekeeper */
